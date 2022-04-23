29cfac7b4f4c6b589c7a1afeb94bd14a055173e0
Gabriel Schenker <gnschenker@gmail.com>
2022-04-23 10:40:19 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => $"Hostname: {Dns.GetHostName()}, Version: 1.1");
+
+app.Run();
